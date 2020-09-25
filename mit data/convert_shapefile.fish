b780013d33e17e7fc1e34e9361b902cdf0d068d1
Cypress Frankenfeld <iamcypress@gmail.com>
2020-09-25 11:46:17 -0400

    Add state boundaries

--- mit data/convert_shapefile.fish
+++ mit data/convert_shapefile.fish
@@ -3,7 +3,9 @@
 shp2json $argv[1] |
 geoproject 'd3.geoAlbersUsa().fitSize([975, 610], d)' |
 ndjson-split 'd.features' |
-ndjson-map 'd.id = d.properties.GEOID.slice(2), d' > temp-map.ndjson
+ndjson-map 'd.id = d.properties.GEOID, d' > temp-map.ndjson
 geo2topo -n counties=temp-map.ndjson |
 toposimplify -p -1 -f |
-topoquantize 1e5 > usa-topo.json
+topoquantize 1e5 |
+topomerge -k 'd.id.slice(0,2)' states=counties |
+topomerge --mesh -f 'a !== b' states=states > usa-topo.json
