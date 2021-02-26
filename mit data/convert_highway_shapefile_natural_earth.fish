5f43256f731a06d9be5bfaa11cb9b3b11262a882
Cypress Frankenfeld <iamcypress@gmail.com>
2021-02-26 16:53:51 -0500

    Add more compact roads data
    This road data can be simplified down to 1MB or so with topojson, instead of 17MB.
    It's from https://www.naturalearthdata.com/downloads/10m-cultural-vectors/roads/
    
    The new script filters out needless features, limits to the USA, and keeps things like
    road name, and what type of highway it is. We can further reduce data size by removing
    the road name and scale-rank if needed.

---
+++ mit data/convert_highway_shapefile_natural_earth.fish
@@ -0,0 +1,37 @@
+#!/usr/local/bin/fish
+
+shp2json $argv[1] |
+geoproject 'd3.geoAlbersUsa().fitSize([975, 610], d)' |
+ndjson-split 'd.features' |
+ndjson-filter 'd.properties.sov_a3 === "USA"' |
+ndjson-map 'delete d.properties.featurecla,
+delete d.properties.sov_a3,
+delete d.properties.note,
+delete d.properties.edited,
+delete d.properties.namealt,
+delete d.properties.namealtt,
+delete d.properties.routeraw,
+delete d.properties.question,
+delete d.properties.length_km,
+delete d.properties.toll,
+delete d.properties.ne_part,
+delete d.properties.label,
+delete d.properties.label2,
+delete d.properties.local,
+delete d.properties.localtype,
+delete d.properties.localalt,
+delete d.properties.labelrank,
+delete d.properties.ignore,
+delete d.properties.add,
+delete d.properties.rwdb_rd_id,
+delete d.properties.orig_fid,
+delete d.properties.prefix,
+delete d.properties.uident,
+delete d.properties.continent,
+delete d.properties.expressway,
+delete d.properties.min_zoom,
+delete d.properties.min_label,
+d' > temp-map.ndjson
+geo2topo -n roads=temp-map.ndjson |
+toposimplify -p -1 -f |
+topoquantize 1e5 > roads-topo.json
