6ac0eddbf120f7bc4c70dcd7b18ebc942ed0ebd0
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-13 17:31:36 -0500

    Add railroad data and increase water data sig figs

---
+++ mit data/convert_railroads.fish
@@ -0,0 +1,17 @@
+#!/usr/local/bin/fish
+
+shp2json $argv[1] |
+geoproject -n 'd3.geoAlbersUsa().scale(1300).translate([487.5, 305])' |
+ndjson-split 'd.features' |
+ndjson-filter 'd.properties.sov_a3 === "USA"' |
+ndjson-map 'delete d.properties.scalerank,
+delete d.properties.featurecla,
+delete d.properties.sov_a3,
+delete d.properties.uident,
+delete d.properties.add,
+delete d.properties.natrlscale,
+delete d.properties.continent,
+d' > temp-map.ndjson
+geo2topo -n railroads=temp-map.ndjson |
+toposimplify -p -1 -f |
+topoquantize 1e5 > railroads-topo.json
