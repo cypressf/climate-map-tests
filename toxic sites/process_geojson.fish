7719be343610ffee517ea39feeb635b07b97647c
Cypress Frankenfeld <iamcypress@gmail.com>
2021-07-23 16:12:14 -0400

    Process to topojson

---
+++ toxic sites/process_geojson.fish
@@ -0,0 +1,6 @@
+#!/usr/local/bin/fish
+cat "texas and louisiana.geojson" |
+    geoproject 'd3.geoAlbersUsa().scale(1300).translate([487.5, 305])' >temp-map.geojson
+geo2topo toxic-sites=temp-map.geojson |
+    toposimplify -p 1 -f |
+    topoquantize 1e5 >toxic-sites-topo.json
